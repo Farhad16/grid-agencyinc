c7729c7620c9068674d78aca82cbab41aeae7fc6
farhad16 <fhossain389@gmail.com>
2025-05-17 13:46:50 +0600

    Setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,82 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import pluginJs from '@eslint/js'
+import typescriptParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
+import pluginReact from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import unicorn from 'eslint-plugin-unicorn'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      importPlugin,
+      unicorn,
+      reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      sourceType: 'module',
+      parser: typescriptParser,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-duplicate-enum-values': 'off',
+      '@typescript-eslint/no-unused-vars': [
         'warn',
-        { allowConstantExport: true },
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allowSingleOrDouble',
+          trailingUnderscore: 'allowDouble',
+        },
+        {
+          selector: ['interface', 'typeAlias'],
+          prefix: ['I'],
+          format: ['StrictPascalCase'],
+        },
+      ],
+      '@typescript-eslint/no-var-requires': 'off',
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
+          ignore: ['vite-env.d.ts'],
+        },
       ],
     },
   },
-)
+  {
+    files: ['src/**/*.stories.*'],
+    rules: {
+      'import/no-anonymous-default-export': 'off',
+    },
+  },
+]
